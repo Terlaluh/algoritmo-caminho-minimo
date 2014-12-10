43c33785998f0fe5e7eaa443c46a774d23222d89
charlesdccti@gmail.com <charlesdccti@gmail.com@94ef538a-c3c7-6784-620b-f42b94a21a03>
2014-12-10 18:40:10 +0000

    projeto no svn pronto

---
+++ trunk/AlgoritmoCaminhoMinimo/src/br/dcc/ufba/grafos/main/Principal.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package br.dcc.ufba.grafos.main;
+
+/**
+ * Objetivo: Determinar os caminhos mínimos entre todos os vértices de um grafo G=(V,E).
+ * Além do algoritmo de Dijkstra, pode-se usar o algoritmo de Floyd-Warshall, de Bellman-Ford, etc.
+ * 
+ * @author Charles
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
